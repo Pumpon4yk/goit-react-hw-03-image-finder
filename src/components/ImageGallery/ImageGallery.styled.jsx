ac53fff6942bd5e8c4946b0a13ceeaca8f7b3715
Pumpon4yk <tt10392@gmail.com>
2023-02-26 16:38:36 +0100

    fix-b

--- src/components/ImageGallery/ImageGallery.styled.jsx
+++ src/components/ImageGallery/ImageGallery.styled.jsx
@@ -3,12 +3,16 @@ import styled from "styled-components"
 
 export const List = styled.ul`
 
-@media  screen and (min-width: 481px) {
-    grid-template-columns: repeat(auto-fill, minmax(380px, 1fr));
+@media  screen and (min-width: 767px) {
+    grid-template-columns: repeat(auto-fill, minmax(calc(100% / 2 - (16px )), 1fr));
+}
+@media  screen and (min-width: 1200px) {
+    grid-template-columns: repeat(auto-fill, minmax(calc(100% / 4 - (16px * 3)), 1fr));
 }
 
     display: grid;
     max-width: calc(100vw - 48px);
+    grid-template-columns: repeat(auto-fill, 1fr);
     grid-gap: 16px;
     margin-top: 0;
     margin-bottom: 0;
